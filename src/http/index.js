9e94d54227330c47e6252837a4adb6acd601dbb0
1nexpl1 <sabiroffarthur@mail.ru>
2024-03-30 19:59:45 +0500

    build corr

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'http://89.108.70.109:5000/'
 })
 
 const $authHost = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'http://89.108.70.109:5000/'
 })
 
 const authInterceptor = config => {
