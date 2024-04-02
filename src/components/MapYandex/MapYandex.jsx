cd818f94b930ac3db3431497170483385a5339ba
Пользователь <1nexpl1>
2024-04-02 19:42:43 +0500

    map

---
+++ src/components/MapYandex/MapYandex.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import s from './MapYandex.module.css'
+const MapYandex = () => {
+    // <div style="position:relative;overflow:hidden;"><a href="https://yandex.ru/maps/org/flowerspro/128645552362/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">FlowersPro</a><a href="https://yandex.ru/maps/20720/verhnyaya-pyshma/category/flower_shop/184108325/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:14px;">Магазин цветов в Верхней Пышме</a><a href="https://yandex.ru/maps/20720/verhnyaya-pyshma/category/flowers_and_bouquets_delivery/184108319/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:28px;">Доставка цветов и букетов в Верхней Пышме</a><iframe src="https://yandex.ru/map-widget/v1/?ll=60.604821%2C56.974560&mode=poi&poi%5Bpoint%5D=60.596286%2C56.975951&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D128645552362&z=16.42" width="560" height="400" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div>
+
+    return (
+        <div className={s.map}>
+            <YMaps>
+                <Map defaultState={{ center: [56.976020, 60.596504], zoom: 15 }}  width = {'100%'} height={'100%'}  >
+                    <Placemark geometry={[56.976020, 60.596504]} />
+                </Map>
+            </YMaps>
+        </div>
+    )
+}
+
+export default MapYandex
--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Title from "../components/UI/Title/Title";
 import Contacts from "../components/Contacts/Contacts";
 import HoverContact from "../components/HoverContact/HoverContact";
+import MapYandex from "../components/MapYandex/MapYandex";
 
 const ContactsPage = () => {
+
   return (
     <div className="contactsWrapper">
       <Title>Контакты</Title>
+      <div className="subTitleContacts">Где мы находимся</div>
+      <MapYandex />
       <div className="subTitleContacts">Свяжитесь с нами удобным способом</div>
       <Contacts />
       <HoverContact/>
