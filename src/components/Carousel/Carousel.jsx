4dd50bdaec0e3b5a2ae7814cd08ebc5c8415ce76
Пользователь <1nexpl1>
2024-05-04 04:45:42 +0500

    phone v6

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -6,6 +6,7 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import { FaAngleLeft } from "react-icons/fa";
 
 const Carousel = (props) => {
+    const mediaQuerry = window.matchMedia('(max-width: 940px)')
     const images = [
         {
             original: props.image,
@@ -24,7 +25,7 @@ const Carousel = (props) => {
             thumbnailWidth: '92px',
         },
 
-        
+
         {
             original: props.image2,
             thumbnail: props.image2,
@@ -50,10 +51,62 @@ const Carousel = (props) => {
             thumbnailWidth: '92px',
         },
     ];
-    
+    const images2 = [
+        {
+            original: props.image,
+            thumbnail: props.image,
+            originalHeight: '300px',
+            originalWidth: '300px',
+            thumbnailHeight: '75px',
+            thumbnailWidth: '75px',
+        },
+        {
+            original: props.image1,
+            thumbnail: props.image1,
+            originalHeight: '300px',
+            originalWidth: '300px',
+            thumbnailHeight: '75px',
+            thumbnailWidth: '75px',
+        },
+
+
+        {
+            original: props.image2,
+            thumbnail: props.image2,
+            originalHeight: '300px',
+            originalWidth: '300px',
+            thumbnailHeight: '75px',
+            thumbnailWidth: '75px',
+        },
+        {
+            original: props.image3,
+            thumbnail: props.image3,
+            originalHeight: '300px',
+            originalWidth: '300px',
+            thumbnailHeight: '75px',
+            thumbnailWidth: '75px',
+        },
+        {
+            original: props.image4,
+            thumbnail: props.image4,
+            originalHeight: '300px',
+            originalWidth: '300px',
+            thumbnailHeight: '75px',
+            thumbnailWidth: '75px',
+        },
+    ];
     return (
-        
-        <div><ImageGallery items={images} thumbnailPosition={'left'} showPlayButton={false} showFullscreenButton={false} /></div>
+        <>
+            {mediaQuerry.matches ? (
+                <div>
+                    <ImageGallery items={images2} thumbnailPosition={'bottom'} showPlayButton={false} showFullscreenButton={false} />
+                </div>
+            ) : (
+                <div>
+                    <ImageGallery items={images} thumbnailPosition={'left'} showPlayButton={false} showFullscreenButton={false} />
+                </div>
+            )}
+        </>
     )
 }
 
