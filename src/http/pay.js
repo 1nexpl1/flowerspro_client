283188957bb18b21ced7340913675ed93c9f8454
Пользователь <1nexpl1>
2024-05-11 18:26:27 +0500

    pay v3

--- src/http/pay.js
+++ src/http/pay.js
@@ -2,10 +2,10 @@ import axios from "axios"
 import md5 from "md5"
 
 export const GoToPay = async(OutSum) => {
-    const {invoiceID} = await axios.post('https://auth.robokassa.ru/Merchant/Indexjson.aspx', {params: {
+    const invoiceID = await axios.post('https://auth.robokassa.ru/Merchant/Indexjson.aspx', {params: {
         'MerchantLogin': 'flowers-pro-vp.ru',
         'OutSum': OutSum,
         'SignatureValue': md5(`flowers-pro-vp.ru:${OutSum}::Theteda123-45`)
         }})
-    return('https://auth.robokassa.ru/Merchant/Index/'+invoiceID)
+    return('https://auth.robokassa.ru/Merchant/Index/'+ invoiceID.invoiceID)
 }
