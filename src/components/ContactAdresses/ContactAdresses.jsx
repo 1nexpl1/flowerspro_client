aa73065998ead97df8061d769761b8f4ff82cae7
Пользователь <1nexpl1>
2024-09-11 02:42:37 +0500

    map update v3

--- src/components/ContactAdresses/ContactAdresses.jsx
+++ src/components/ContactAdresses/ContactAdresses.jsx
@@ -1,22 +1,48 @@
 import React from 'react'
 import s from './ContactAdresses.module.css'
+import sekb from '../../images/salonekb.jpg'
+import svp from '../../images/salonvp.jpg'
+import { FaWhatsapp } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
+import { FaMapMarkerAlt } from "react-icons/fa";
 const ContactAdresses = () => {
   return (
+    <div className={s.mainWrapper}>
     <div className={s.wrapper}>
+      <div className={s.itemWrapper}>
+        <img className={s.photo} src={sekb} alt="" />
+        <div className={s.textWrapper}>
         <div className={s.title}>Екатеринбург</div>
-        <div className={s.subtitle}>Номер телефона</div>
-        <div className={s.text}>+7(912)66-44-689</div>
-        <div className={s.subtitle}>Адрес</div>
+        <div className={s.addres}>
+        <FaMapMarkerAlt />
         <div className={s.text}>г.Екатеринбург, улица Большакова, дом 143</div>
-        <div className={s.subtitle}>Время Работы</div>
-        <div className={s.text}>Ежедневно с 9 до 21</div>
+        </div>
+        <div className={s.number}>
+        <FaPhone />
+        <FaWhatsapp />
+        <div className={s.text}>+7(912)66-44-689</div>
+        </div>
+        </div>
+      </div>
+      <div className={s.itemWrapper}>
+        <img className={s.photo} src={svp} alt="" />
+        <div className={s.textWrapper}>
         <div className={s.title}>Верхняя Пышма</div>
-        <div className={s.subtitle}>Адрес</div>
+        <div className={s.addres}>
+        <FaMapMarkerAlt />
         <div className={s.text}>г.Верхняя пышма, улица Петрова, дом 13А</div>
-        <div className={s.subtitle}>Время Работы</div>
-        <div className={s.text}>Ежедневно с 9 до 21</div>
-        <div className={s.subtitle}>Номер телефона</div>
+        </div>
+        <div className={s.number}>
+        <FaPhone />
+        <FaWhatsapp />
         <div className={s.text}>+7(912)66-44-096</div>
+        </div>
+        </div>
+      </div>
+    </div>
+      <div className={s.Time}>
+        Ежедневно с 9:00 до 21:00
+      </div>
     </div>
   )
 }
